b693aab05da01c43b3d2dd3ff58a7933baee68ab
Nakade Shoya <doshouya@gmail.com>
2022-04-09 08:42:51 +0900

    Initial Commit

---
+++ WhichIs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhichIs
+//
+//  Created by 中出翔也 on 2022/04/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WhichIs/WhichIsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhichIsApp.swift
+//  WhichIs
+//
+//  Created by 中出翔也 on 2022/04/09.
+//
+
+import SwiftUI
+
+@main
+struct WhichIsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
